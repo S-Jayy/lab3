8e73f6f3bf667993075bdb6ffa735e1365c56851
Sandro Jay Pardines <sdpardines@student.apc.edu.ph>
2024-03-09 11:27:38 +0800

    Update

--- ci4/app/Views/pages/home.php
+++ ci4/app/Views/pages/home.php
@@ -14,7 +14,6 @@
     <title>Introduction - Sandro Jay Pardines</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
-        /* Updated CSS */
         body {
             font-family: Arial, sans-serif;
             background-color: #f4f4f4;
